213ae37de786b9a0c2900e223fbdb687eeb8b00a
Sigurdur Njalsson <siggnja@gmail.com>
2016-10-11 20:05:08 +0000

    Calculator now passed test for one negative number by throwing illegalargument exception

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,33 +2,61 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
+	public static int add(String text)
+	{
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(","))
+		{
+			testNegativity(splitNumbers(text));
 			return sum(splitNumbers(text));
 		}
 		else
 			return 1;
 	}
 
-	private static int toInt(String number){
+	private static int toInt(String number)
+	{
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
+	private static String[] splitNumbers(String numbers)
+	{
 	    return numbers.split(",|\n");
 	}
       
-    private static int sum(String[] numbers){
+    private static int sum(String[] numbers)
+    {
  	    int total = 0;
-        for(String number : numbers){
+        for(String number : numbers)
+        {
 		    total += toInt(number);
 		}
 		return total;
     }
 
-
+    private static void testNegativity(String[] numbers)
+   	{
+   		String result[] = new String[numbers.length];
+   		int count = 0;
+   		for(String temp : numbers)
+   		{
+   			if(toInt(temp) < 0)
+   			{
+   				result[count] = temp;
+   				count++;
+   			}
+   		}
+   		if(count > 0)
+   		{
+   			String message ="Negative not allowed: ";
+   			for(String temp : result)
+   			{
+   				message += temp;
+   			}
+   			throw new IllegalArgumentException(message);
+   		}
+   	}
 
 }

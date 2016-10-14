f4e5679379df8106c6bc98e321dd088d8688112e
Sigurdur Njalsson <siggnja@gmail.com>
2016-10-14 20:17:42 +0000

    small refactoring with comments on functions

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -29,6 +29,7 @@ public class Calculator
 		  testNegativity(numbers.split(",|\n"));
 	    return numbers.split(",|\n");
 	}
+  //Splits by special delimeter given //[delimeter]\n[numbers]
   private static String[] splitSpecialDelimNumbers(String numbers)
   {
     	String[] res = numbers.split("\n",2);
@@ -36,42 +37,45 @@ public class Calculator
 		  testNegativity(res[1].split(special));
 		  return res[1].split(special);
   }
-    private static int sum(String[] numbers)
+  //sums up the values
+  private static int sum(String[] numbers)
+  {
+ 	  int total = 0;
+    for(String number : numbers)
     {
- 	    int total = 0;
-        for(String number : numbers)
-        {
-        	int res = toInt(number);
-			if(res <= 1000) total += toInt(number);
-		}
+        int res = toInt(number);
+		if(res <= 1000) total += toInt(number);
+	  }
 		return total;
-    }
-
-    private static void testNegativity(String[] numbers)
+  }
+  //Test for negative number and creates an informative error message
+  private static void testNegativity(String[] numbers)
+  {
+   	String result[] = new String[numbers.length];
+   	int count = 0;
+   	for(String temp : numbers)
    	{
-   		String result[] = new String[numbers.length];
-   		int count = 0;
-   		for(String temp : numbers)
+   		if(toInt(temp) < 0)
    		{
-   			if(toInt(temp) < 0)
-   			{
    				result[count++] = temp;
-   			}
-   		}
-   		if(count > 0)
-   		{
-   			String message ="Negative not allowed: " + result[0];
-   			if(count > 1)
-   			{
-
-	   			for(String temp : result)
-   				{
-   					if(temp == result[0]) continue;
-
-   					message += "," + temp;
-   				}
-   			}
-   			throw new IllegalArgumentException(message);
    		}
    	}
+   	if(count > 0)
+   	{
+   		String message ="Negative not allowed: " + result[0];
+   		if(count > 1)
+   		{
+
+	   		for(String temp : result)
+   			{
+   				if(temp == result[0]) continue;
+
+   					message += "," + temp;
+   			}
+   		}
+   			throw new IllegalArgumentException(message);
+   	}
+  }
+
+
 }
